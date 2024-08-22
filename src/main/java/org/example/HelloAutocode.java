18873938530bbd30dc012be49f5198d3d1b9f6a6
Yehor Zubchenko <zucbhik1995@gmail.com>
2024-08-22 20:09:27 +0300

    task1 epam

--- src/main/java/org/example/HelloAutocode.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class HelloAutocode {
-    public static void main(String[] args) {
-        System.out.println("Hello,Autocode!");
-    }
-}
---
+++ src/main/java/org/example/MeetAStranger.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        System.out.println("Hello, " + str);
+    }
+}
